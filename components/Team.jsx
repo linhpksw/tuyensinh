375d2497d3ba4fd5c8a5f0a02540686f87a7f07b
Trong Linh <linhpksw@gmail.com>
2023-05-14 14:37:26 +0700

    change teacher image

--- components/Team.jsx
+++ components/Team.jsx
@@ -1,24 +1,24 @@
 import Image from "next/image"
 import Container from "./Container"
 import giaovien1 from '../public/img/giaovien-1.jpg'
+import giaovien2 from '../public/img/giaovien-2.jpg'
+import giaovien3 from '../public/img/giaovien-3.jpg'
+
 const Team = () => {
     const people = [
         {
             name: 'Thầy Đinh Hữu Lâm',
-            role: 'Chức vụ',
-            src: giaovien1,
+            src: giaovien3,
             bio: 'Cựu học sinh chuyên toán khối phổ thông chuyên toán Sư Phạm; Cựu sinh viên lớp chất lượng cao K50 trường ĐHSP Hà Nội; Thạc sĩ chuyên ngành hình học trường ĐH KHTN; giải nhất Giáo viên giỏi thành phố Hà Nội 2014; tổ trưởng tổ Toán trường THPT chuyên Nguyễn Huệ, chuyên viên môn toán khối THPT Sở GDĐT Hà Nội, bồi dưỡng nhiều học sinh giỏi quốc gia.',
         },
         {
             name: 'Thầy Phùng Văn Mạnh',
-            role: 'Chức vụ',
             src: giaovien1,
-            bio: 'Cựu học sinh chuyên toán trường chuyên Hưng Yên, giải NHÌ quốc gia môn toán; lớp trưởng lớp chất lượng cao K50 trường ĐHSP Hà Nội; học bổng toàn phần làm tiến sĩ khoa học tại Pháp; giảng viên tổ giải tích trường ĐH SPHN, năm 2017 thầy được vinh dự phong chức danh PGS ở tuổi 35.',
+            bio: 'Cựu học sinh chuyên toán trường chuyên Hưng Yên, giải NHÌ quốc gia môn toán; lớp trưởng lớp chất lượng cao K50 trường ĐHSP Hà Nội; học bổng toàn phần làm tiến sĩ khoa học tại Pháp; giảng viên tổ giải tích trường ĐHSP Hà Nội, năm 2017 thầy được vinh dự phong chức danh PGS ở tuổi 35.',
         },
         {
             name: 'Cô Đặng Thị Hường',
-            role: 'Chức vụ',
-            src: giaovien1,
+            src: giaovien2,
             bio: 'Giáo viên toán trường THPT chuyên Nguyễn Huệ, luyện thi vào 10 và thi THPTQG lâu năm.',
         },
 
@@ -31,12 +31,11 @@ const Team = () => {
                     people.map((person) =>
                         <div key={person.name} className="md:flex gap-10 pb-6 md:pb-12 pt-[10px] md:pt-[1px] border-b-[1px] md:tracking-wide">
                             <div className=" md:w-1/3 lg:w-1/4">
-                                <Image src={giaovien1} className="object-cover max-w-[150px] md:max-w-full rounded-lg shadow-lg" alt='teacher' />
+                                <Image src={person.src} className="object-cover max-w-[150px] md:max-w-full rounded-lg shadow-lg" alt='teacher' />
                             </div>
 
                             <div className="mt-10 md:mt-2 md:w-2/3 lg:w-3/4">
                                 <h3 className="text-2xl font-medium leading-6 mb-3 text-slate-700">{person.name}</h3>
-                                <p className="leading-6 text-lg text-indigo-600">{person.role}</p>
 
                                 <p className="text-gray-500 mt-5 leading-loose text-lg">{person.bio}</p>
                             </div>
--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
     return (
         <Html lang='vn'>
-            <Head>{/* <script src='/js/flowbite.min.js' defer></script> */}</Head>
+            <Head>
+                <script src='/js/flowbite.min.js' defer></script>
+            </Head>
             <body>
                 <Main />
                 <NextScript />
