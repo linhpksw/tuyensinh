0f6290dd205af9b6471c2b925f64ce0b07fe93df
Trong Linh <linhpksw@gmail.com>
2023-05-14 17:00:49 +0700

    add og:img

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
     return (
         <Html lang='vn'>
             <Head>
+                <meta property='og:image' content='../public/img/hsg-1.jpg' />
                 <script src='/js/flowbite.min.js' defer></script>
             </Head>
             <body>
