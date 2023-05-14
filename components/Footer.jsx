2a4df201e18a1df6979288a4d09ed3144d367992
Trong Linh <linhpksw@gmail.com>
2023-05-14 20:27:14 +0700

    update address and phone number

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,6 +2,9 @@ import Container from "./Container"
 import logoImg from '../public/img/logo.svg'
 import Image from "next/image"
 
+import { MapPinIcon, PhoneIcon } from "@heroicons/react/24/solid"
+import Link from "next/link"
+
 const Footer = () => {
     const navigation = [
         { name: 'About', href: '#' },
@@ -25,10 +28,30 @@ const Footer = () => {
                 ))}
             </nav> */}
             <div className="mb-6">
-                <div className="flex justify-center">
-                    <Image src={logoImg} alt="logo" width={250} height={250} />
+                <h3 className="text-2xl md:text-3xl font-semibold flex justify-center mb-4 uppercase text-gray-600">Thông tin lớp học</h3>
+
+                <div className="flex flex-col gap-5 md:flex-row md:justify-center md:items-center md:gap-20 mb-10 md:mb-20">
+
+                    <div className="flex items-start md:items-center gap-2">
+                        <MapPinIcon className="w-10 h-10 md:w-6 md:h-6 text-indigo-500" />
+
+                        <p className="text-gray-500">
+                            Địa chỉ: <Link className="text-indigo-500 hover:text-indigo-400" href='https://goo.gl/maps/proqtNoL24gvuNxy9'>Trường THPT DL Lê Hồng Phong - số 27 Tô Hiệu, Hà Đông</Link>
+                        </p>
+                    </div>
+
+                    <div className="flex items-center gap-2">
+                        <PhoneIcon className="w-6 h-6 text-indigo-500" />
+
+                        <p className="text-gray-500">Điện thoại: <a href="tel:0362860970" className="text-indigo-500 hover:text-indigo-400 whitespace-nowrap">036 286 0970</a></p>
+                    </div>
                 </div>
-                <p className="mt-4 text-center text-sm text-gray-400">&copy; All rights reserved.</p>
+
+
+                <div className="flex justify-center">
+                    <Image src={logoImg} alt="logo" width={300} height={300} />
+                </div>
+                <p className="mt-1 text-center text-gray-400">&copy; All rights reserved.</p>
             </div>
 
         </Container>
