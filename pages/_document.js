c6a5149cacfabe8999836abab0b25b2546db6f65
Trong Linh <linhpksw@gmail.com>
2023-05-14 17:15:25 +0700

    Add more og properties

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,11 @@ export default function Document() {
         <Html lang='vn'>
             <Head>
                 <meta property='og:image' content='https://tuyensinh.clbanhsang.com/img/hsg-1.jpg' />
+
+                <meta property='og:url' content='https://tuyensinh.clbanhsang.com/' />
+                <meta property='og:type' content='website' />
+                <meta property='og:title' content='Câu lạc bộ Toán Ánh Sáng' />
+                <meta property='og:description' content='Trung tâm học toán chất lượng cao từ lớp 8 đến lớp 12' />
                 <script src='/js/flowbite.min.js' defer></script>
             </Head>
             <body>
